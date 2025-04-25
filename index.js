571a632f4a37dc3cda908b9755709ac95d405898
mazharul90007 <mazharul90007@gmail.com>
2025-04-25 19:39:03 +0600

    server updated

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ const verifyToken = (req, res, next) => {
 
 async function run() {
   try {
-    // await client.connect();
+    await client.connect();
     // Send a ping to confirm a successful connection
     const userCollection = client.db("ezyTicket").collection("users");
     const eventCollection = client.db("ezyTicket").collection("events");
@@ -857,10 +857,10 @@ async function run() {
     })
     // ------------- Stripe Payment----------
 
-    // await client.db("admin").command({ ping: 1 });
-    // console.log(
-    //   "Pinged your deployment. You successfully connected to MongoDB!"
-    // );
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error.
     // await client.close();
